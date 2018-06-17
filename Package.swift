b5623ab335b3db658f436cab16a8bf15d28c14ff
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:29:27 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Cairo",
     dependencies: [
         .Package(url: "https://github.com/viachpaliy/CCairo.git", majorVersion: 1),
-        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 2),
+        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git",  majorVersion: 1),
     ],
     swiftLanguageVersions: [3, 4]
 )
