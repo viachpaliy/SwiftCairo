ae417441c2aef8cf97a4c2c5b3fceb8f80212a8f
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:22:46 +0300

    change repository from original to fork

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Cairo",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CCairo.git", majorVersion: 1),
-        .Package(url: "https://github.com/rhx/SwiftGObject.git", majorVersion: 2),
+        .Package(url: "https://github.com/viachpaliy/CCairo.git", majorVersion: 1),
+        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 2),
     ],
     swiftLanguageVersions: [3, 4]
 )
